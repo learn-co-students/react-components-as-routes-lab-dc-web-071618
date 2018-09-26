6fde7704737ee125fde9a5073dc6af9603dde917
john-raber <jrab224@gmail.com>
2018-09-26 14:57:02 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(a => {
+        return (
+          <div className="actor">
+            <h3>{a.name}</h3>
+            <ul>
+              {a.movies.map(m => {
+                return <li>{m}</li>;
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
